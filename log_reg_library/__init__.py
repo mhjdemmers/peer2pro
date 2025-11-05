88a18fe39422be0e859fadccf67fda16e223f4cc
Benjamin Soudant <b.soudant1803@gmail.com>
2025-11-05 13:53:05 +0100

    library log_reg

---
+++ log_reg_library/__init__.py
@@ -0,0 +1,3 @@
+from .log_reg_library import *
+
+__all__ = ["log_reg_library"]
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from matching import MatchingEngine
-
+from log_reg_library import NLPMinorPredictor
 
 def main() -> None:
+    classificator = NLPMinorPredictor(
+        model_path="nlp_model_logreg_embeddings.pkl",
+        label_encoder_path="label_encoder_log_reg.pkl",
+        embed_model_name="paraphrase-multilingual-mpnet-base-v2"
+    )
+
+    classificator.predict_csv
+    (
+        csv_path="./DATASETS/opdrachten_te_classificeren.csv",
+        output_path="./DATASETS/voorspellingen_output_log_reg.csv"
+    )
+
     engine = MatchingEngine(
         students_path="./DATASETS/studenten.csv",
         mentors_type1_path="./DATASETS/mentoren.csv",
